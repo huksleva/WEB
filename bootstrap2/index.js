619723392967b397e36a8fc24df40f1055b3fdc6
Leonid Tots <59145792+huksleva@users.noreply.github.com>
2025-10-23 12:53:04 +0300

    123

---
+++ bootstrap2/index.js
@@ -0,0 +1,8 @@
+// Импорт стилей Bootstrap (только если вы используете сборщик или браузер поддерживает импорт CSS — см. ниже!)
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Импорт JS-компонентов Bootstrap (модалки, тултипы и т.д.)
+import 'bootstrap';
+
+// Импорт Luxon
+import { DateTime } from 'luxon';
